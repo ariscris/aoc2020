1f9aff5e9cb97cc3cde824782c1f42f160e04d3b
Aris Villareal <maricriv@amazon.com>
2020-12-08 17:48:33 -0500

    day8 gold

--- day8/day8.ts
+++ day8/day8.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 
 console.log(`sample: ${calc(load('./sample.txt'))}`)
 console.log(`input: ${calc(load('./input.txt'))}`)
+console.log(`sample fix: ${fix(load('./sample.txt'), 0, 0, true)}`)
+console.log(`input fix: ${fix(load('./input.txt'), 0, 0, true)}`)
 
 function load(fileName: string): Array<{ command: string, delta: number }> {
   const commands = new Array<{ command: string, delta: number }>();
@@ -34,3 +36,32 @@ function calc(commands: Array<{ command: string, delta: number }>) {
   return total;
 }
 
+function fix(commands: Array<{ command: string, delta: number }>, currPos: number, total: number, allowSwap: boolean) {
+  const visited = new Set<number>();
+  while (!visited.has(currPos)) {
+    console.log(currPos)
+    if (currPos > commands.length - 1)
+      return total;
+
+    const {command, delta} = commands[currPos];
+    visited.add(currPos);
+
+    if (allowSwap && (command === 'nop' || command === 'jmp')) {
+      const commandCopy = commands.map((x) => x);
+      if (command === 'nop')
+        commandCopy[currPos] = { command:'jmp', delta };
+      else 
+        commandCopy[currPos] = { command:'nop', delta };
+
+      const result = fix(commandCopy, currPos, total, false);
+      if (result)
+        return result;
+    }
+
+    switch(command) {
+      case 'acc': total = total + delta;
+      case 'nop': currPos = currPos + 1; break;
+      case 'jmp': currPos = currPos + delta; break;
+    }
+  }
+}
