47956b8c6cfdeefe428b324944bb31c8d9d135be
Aris Villareal <maricriv@amazon.com>
2020-12-10 22:33:06 -0500

    day10 trying for gold

--- day10/day10.ts
+++ day10/day10.ts
@@ -1,7 +1,9 @@
 import * as fs from 'fs';
 
+console.log({smallsample: calc(parse('./smallsample.txt'))});
 console.log({sample: calc(parse('./sample.txt'))});
 console.log({input: calc(parse('./input.txt'))});
+console.log({smallsample: count(parse('./smallsample.txt'))});
 
 function calc(nums: Array<number>) {
   nums.push(nums[nums.length-1] + 3)
@@ -20,6 +22,23 @@ function calc(nums: Array<number>) {
     curr = n;
   }
   return countDelta1 * countDelta3;
+}
+
+function count(nums: Array<number>) {
+  nums.push(nums[nums.length-1] + 3)
+  let curr = 0;
+  let count = 1;
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    console.log({i, n, curr, count, next1: nums[i+1], next2: nums[i+2]})
+    if (i + 1 < nums.length && nums[i+1] - curr <= 3 )
+      count++;
+    if (i + 2 < nums.length && nums[i+2] - curr <= 3 )
+      count++;
+
+    curr = n;
+  }
+  return count;
 }
 
 function parse(fileName: string): Array<number> {
